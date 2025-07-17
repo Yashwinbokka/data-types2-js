7e71399391dc112c9c934ea65c0c521ec6e7a7bd
Yashwinbokka <yashwin2094@gmail.com>
2025-07-17 09:32:42 +0530

    Add files via upload

---
+++ value.js
@@ -0,0 +1,104 @@
+/*console.log("hello world");
+console.log(100+100);
+
+console.log(true);
+console.log(false);
+console.log([5,6,7,8,9]);
+
+var rosy = "singing"
+
+console.log(rosy);
+var rosy = "dancing"
+console.log(rosy);
+
+let school = 'kindergarten'
+school = "primary"
+console.log(school);
+
+const ground = 'stadium'
+console.log(ground);*/
+
+/*console.log("hello world");
+console.log("Deva bhaiiiiiiiiiii");
+console.log( 0*0);
+console.log({name:"Devaaaaaaaaaaaaaaaaaa", age: 100});
+var  username = 'yashwin'
+console.log(username);
+let cars = ["ford", "bmw", "benz",{name2:"heLLO", age:200}]
+console.log(cars);
+
+let ipl = {
+    cricket:"csk",
+    football:"rcb",
+    basketball:"MI"
+}
+console.log(ipl)
+}*/
+
+/*const number = BigInt(6378467483);
+    console.log(number);
+
+function greet(){
+    console.log("hello world");
+}
+greet();*/
+
+/*Date
+const today = new Date();
+console.log("Today's date is",tomorrow);
+
+const map = new map();
+map.set("yashwin");
+console.log(map);
+
+const set = new set();
+set.add('Hyderabad');
+set.add('delhi');
+set.add('mumbai');
+set.add('kolkota');
+console.log("set:", set);*/
+
+/*let x;
+console.log("value of x is", x)
+
+let y = null;
+console.log("value of y is", y);
+
+let sym1 = Symbol("id");
+let sym2 = Symbol("id");
+console.log( sym1===sym2);
+
+console.log("false");
+console.log("true");
+console.log({name:"yashwin", age: 20});object
+
+/*Date
+new Date().toLocaleDateString();
+const currentdate = new Date();
+console.log(currentdate.toLocaleDateString);
+console.log(currentdate.getFullYear());*/
+
+/*const mymap = new Map();
+map.set("name", "yashwin");
+map.set("course","iot");
+console.log("Map",map); */
+
+String = "yashwin"
+console.log(String);
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
